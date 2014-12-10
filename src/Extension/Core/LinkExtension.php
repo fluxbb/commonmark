87bab2804b2710e2dfcda8737d4546887d727728
Franz Liedke <franz@develophp.org>
2014-12-10 11:05:51 +0100

    Allow all protocols from the spec in autolinks.
    Refs #1.

--- src/Extension/Core/LinkExtension.php
+++ src/Extension/Core/LinkExtension.php
@@ -196,7 +196,8 @@ class LinkExtension implements ExtensionInterface, RendererAwareInterface
             return;
         }
 
-        $text->replace('{<((?:https?|ftp):[^\'">\s]+)>}', function (Text $w, Text $url) {
+        $protocols = implode('|', $this->getValidProtocols());
+        $text->replace('{<((?:'.$protocols.'):[^\'">\s]+)>}', function (Text $w, Text $url) {
             $this->markdown->emit('escape.special_chars', [$url->replace('/(?<!\\\\)_/', '\\\\_')]);
 
             return $this->getRenderer()->renderLink($url, [
@@ -243,6 +244,35 @@ class LinkExtension implements ExtensionInterface, RendererAwareInterface
 
             return $this->getRenderer()->renderLink($text, ['href' => $address]);
         });
+    }
+    
+    protected function getValidProtocols()
+    {
+        return [
+            'coap', 'doi', 'javascript', 'aaa', 'aaas', 'about', 'acap', 'cap',
+            'cid', 'crid', 'data', 'dav', 'dict', 'dns', 'file', 'ftp', 'geo', 'go',
+            'gopher', 'h323', 'http', 'https', 'iax', 'icap', 'im', 'imap', 'info',
+            'ipp', 'iris', 'iris.beep', 'iris.xpc', 'iris.xpcs', 'iris.lwz', 'ldap',
+            'mailto', 'mid', 'msrp', 'msrps', 'mtqp', 'mupdate', 'news', 'nfs',
+            'ni', 'nih', 'nntp', 'opaquelocktoken', 'pop', 'pres', 'rtsp',
+            'service', 'session', 'shttp', 'sieve', 'sip', 'sips', 'sms', 'snmp',
+            'soap.beep', 'soap.beeps', 'tag', 'tel', 'telnet', 'tftp', 'thismessage',
+            'tn3270', 'tip', 'tv', 'urn', 'vemmi', 'ws', 'wss', 'xcon',
+            'xcon-userid', 'xmlrpc.beep', 'xmlrpc.beeps', 'xmpp', 'z39.50r',
+            'z39.50s', 'adiumxtra', 'afp', 'afs', 'aim', 'apt',' attachment', 'aw',
+            'beshare', 'bitcoin', 'bolo', 'callto', 'chrome',' chrome-extension',
+            'com-eventbrite-attendee', 'content', 'cvs',' dlna-playsingle',
+            'dlna-playcontainer', 'dtn', 'dvb', 'ed2k', 'facetime', 'feed',
+            'finger', 'fish', 'gg', 'git', 'gizmoproject', 'gtalk', 'hcp', 'icon',
+            'ipn', 'irc', 'irc6', 'ircs', 'itms', 'jar', 'jms', 'keyparc', 'lastfm',
+            'ldaps', 'magnet', 'maps', 'market',' message', 'mms', 'ms-help',
+            'msnim', 'mumble', 'mvn', 'notes', 'oid', 'palm', 'paparazzi',
+            'platform', 'proxy', 'psyc', 'query', 'res', 'resource', 'rmi', 'rsync',
+            'rtmp', 'secondlife', 'sftp', 'sgn', 'skype', 'smb', 'soldat',
+            'spotify', 'ssh', 'steam', 'svn', 'teamspeak', 'things', 'udp',
+            'unreal', 'ut2004', 'ventrilo', 'view-source', 'webcal', 'wtai',
+            'wyciwyg', 'xfire', 'xri', 'ymsgr',
+        ];
     }
 
     /**
