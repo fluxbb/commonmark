d6b18d05c6414c0f3d47572a4ef114f5035e59ae
Franz Liedke <franz@develophp.org>
2014-12-14 21:55:31 +0100

    Doc comments.

--- src/Node/Block.php
+++ src/Node/Block.php
@@ -79,10 +79,14 @@ abstract class Block extends Node implements NodeAcceptorInterface
         return $this->parent->acceptCodeBlock($codeBlock);
     }
 
-    /*
-     * Node visitor methods
+    /**
+     * Accept a visit from a node visitor.
+     *
+     * This method should instrument the visitor to handle this node correctly, and also pass it on to any child nodes.
+     *
+     * @param NodeVisitorInterface $visitor
+     * @return void
      */
-
     public function visit(NodeVisitorInterface $visitor)
     {
         foreach ($this->children as $child)
--- src/Node/Node.php
+++ src/Node/Node.php
@@ -16,25 +16,70 @@ abstract class Node implements NodeAcceptorInterface
     protected $parent = null;
 
 
+    /**
+     * Return a string representation of this node's type.
+     *
+     * @return string
+     */
     abstract public function getType();
 
+    /**
+     * Convert this node to a string.
+     *
+     * @return string
+     */
     public function toString()
     {
         return $this->getType();
     }
 
+    /**
+     * Accept a visit from a node visitor.
+     *
+     * This method should instrument the visitor to handle this node correctly, and also pass it on to any child nodes.
+     *
+     * @param NodeVisitorInterface $visitor
+     * @return void
+     */
     abstract public function visit(NodeVisitorInterface $visitor);
 
+    /**
+     * Set a node as parent of this node.
+     *
+     * @param Node $parent
+     * @return void
+     */
     public function setParent(Node $parent)
     {
         $this->parent = $parent;
     }
 
+    /**
+     * Get the parent of this node.
+     *
+     * @return Node
+     */
+    public function getParent()
+    {
+        return $this->parent;
+    }
+
+    /**
+     * Get all children of this node, if any.
+     *
+     * @return Node[]
+     */
     public function getChildNodes()
     {
         return $this->children;
     }
 
+    /**
+     * Add a node as child of this node.
+     *
+     * @param Node $child
+     * @return $this
+     */
     public function addChild(Node $child)
     {
         $this->children[] = $child;
@@ -43,6 +88,12 @@ abstract class Node implements NodeAcceptorInterface
         return $this;
     }
 
+    /**
+     * Remove the given node from the list of child nodes.
+     *
+     * @param Node $child
+     * @return void
+     */
     public function removeChild(Node $child)
     {
         $this->children = array_filter($this->children, function (Node $element) use ($child) {
@@ -50,17 +101,15 @@ abstract class Node implements NodeAcceptorInterface
         });
     }
 
+    /**
+     * Merge the given node's children into the current one's.
+     *
+     * @param Node $sibling
+     * @return void
+     */
     public function merge(Node $sibling)
     {
         $this->children = array_merge($this->children, $sibling->children);
-    }
-
-    /**
-     * @return Node
-     */
-    public function getParent()
-    {
-        return $this->parent;
     }
 
 }
