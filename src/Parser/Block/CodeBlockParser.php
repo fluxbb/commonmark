dcfae29fac262aa8ac78abe56a34d4ae4d8cd936
Franz Liedke <franz@develophp.org>
2015-01-07 15:23:04 +0100

    Improve indented code block parsing.

--- src/Parser/Block/CodeBlockParser.php
+++ src/Parser/Block/CodeBlockParser.php
@@ -24,20 +24,24 @@ class CodeBlockParser extends AbstractBlockParser
     {
         $content->handle(
             '{
-                (?:\n\n|\A)
-                (                      # $1 = the code block -- one or more lines, starting with at least four spaces
-                  (?:
-                    (?:[ ]{4})         # Lines must start with four spaces
-                    .*\n+
-                  )+
-                )
-                (?:(?=^[ ]{0,4}\S)|\Z) # Lookahead for non-space at line-start, or end of doc
+                ^
+                (?:(?<=\n\n)|\A)    # Ensure blank line before (or beginning of subject)
+                [ ]{4}              # four leading spaces
+                .+
+                (?:                 # optionally more spaces
+                    (?:             # blank lines in between are okay
+                        \n[ ]*
+                    )*
+                    \n
+                    [ ]{4}
+                    .+
+                )*
+                $
             }mx',
-            function (Text $whole, Text $code) use ($target) {
-                // TODO: Prepare contents
-
+            function (Text $code) use ($target) {
                 // Remove indent
-                $code->replace('/^(\t|[ ]{1,4})/m', '');
+                $code->replace('/^[ ]{1,4}/m', '');
+                $code->append("\n");
 
                 $target->acceptCodeBlock(new CodeBlock($code));
             },
